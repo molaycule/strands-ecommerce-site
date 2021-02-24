ac2ec35f64e0e1aa349adf9ec3351dbaf2438b5f
Mohammed Agboola <magboola@inlaks.com>
2021-02-24 17:16:17 +0100

    task: route loader with nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,25 @@
 import type { AppProps /*, AppContext */ } from 'next/app';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import 'fontawesome';
 import 'styles/main.css';
 import 'styles/utils.css';
+
+NProgress.configure({
+  minimum: 0.3,
+  easing: 'ease',
+  speed: 800,
+  showSpinner: false
+});
+
+//Binding events.
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,14 @@ import TopCategories from 'components/categories/TopCategories';
 import ProductOverview from 'components/products/ProductOverview';
 import ProductItemModal from 'components/products/ProductItemModal';
 import PageWrapper from 'components/common/PageWrapper';
-import Page from 'react-page-loading';
 
 export default function Home() {
   return (
-    <Page loader='bubble' color='#c56439' size={3}>
-      <PageWrapper isHomePage>
-        <BannerSlide />
-        <TopCategories />
-        <ProductOverview />
-        <ProductItemModal />
-      </PageWrapper>
-    </Page>
+    <PageWrapper isHomePage>
+      <BannerSlide />
+      <TopCategories />
+      <ProductOverview />
+      <ProductItemModal />
+    </PageWrapper>
   );
 }
