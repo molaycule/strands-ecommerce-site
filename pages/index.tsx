5f5eb523e0097018c847638a790066c633b270e6
Mohammed Agboola <magboola@inlaks.com>
2021-02-24 18:23:10 +0100

    task: added react spring

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,19 @@ import TopCategories from 'components/categories/TopCategories';
 import ProductOverview from 'components/products/ProductOverview';
 import ProductItemModal from 'components/products/ProductItemModal';
 import PageWrapper from 'components/common/PageWrapper';
+import { useSpring, animated } from 'react-spring';
 
 export default function Home() {
+  const animProps = useSpring({ opacity: 1, from: { opacity: 0 } });
+
   return (
     <PageWrapper isHomePage>
-      <BannerSlide />
-      <TopCategories />
-      <ProductOverview />
-      <ProductItemModal />
+      <animated.div style={animProps}>
+        <BannerSlide />
+        <TopCategories />
+        <ProductOverview />
+        <ProductItemModal />
+      </animated.div>
     </PageWrapper>
   );
 }
