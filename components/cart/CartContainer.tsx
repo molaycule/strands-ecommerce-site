0d5874e343724fafdd1cdc01efb65a58510d8a14
moodele <moh.agboola@gmail.com>
2021-03-20 07:44:00 +0100

    task: Cart Checkout Component

--- components/cart/CartContainer.tsx
+++ components/cart/CartContainer.tsx
@@ -1,17 +1,15 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import dynamic from 'next/dynamic';
-import Select from 'react-select';
 import { useCartStore } from 'store/useCartStore';
-import { useShippingStore } from 'store/useShippingStore';
-import { useQuery } from '@apollo/client';
-import { AllShippings } from 'types';
-import { ALL_SHIPPINGS } from 'graphql/queries';
-import { FC, useEffect, useState } from 'react';
-import { SelectOptions } from 'types';
+import { FC } from 'react';
 import Link from 'next/link';
 import routes from 'routes';
 
-const CartTable = dynamic(() => import('components/cart/CartTable'), {
+const CartTable = dynamic(() => import('./CartTable'), {
+  ssr: false
+});
+
+const CartCheckout = dynamic(() => import('./CartCheckout'), {
   ssr: false
 });
 
@@ -21,61 +19,6 @@ export interface CartContainerProps {
 
 const CartContainer: FC<CartContainerProps> = ({ checkout }) => {
   const cart = useCartStore(state => state.cart);
-  const getCartTotalPrice = useCartStore(state => state.getCartTotalPrice);
-  const shippingDetails = useShippingStore(state => state.shippingDetails);
-  const updateShippingDetails = useShippingStore(
-    state => state.updateShippingDetails
-  );
-  const { data } = useQuery<AllShippings>(ALL_SHIPPINGS);
-  const [shippingByCountry, setShippingByCountry] = useState({});
-  const [selectedCountry, setSelectedCountry] = useState<string>(
-    shippingDetails?.country || ''
-  );
-  const [selectedState, setSelectedState] = useState(
-    shippingDetails?.state || ''
-  );
-  const [selectedStateFee, setSelectedStateFee] = useState(
-    shippingDetails?.fee || 0
-  );
-  const [address, setAddress] = useState<string>(
-    shippingDetails?.address || ''
-  );
-  const [email, setEmail] = useState<string>(shippingDetails?.email || '');
-  const [phone, setPhone] = useState<string>(shippingDetails?.phone || '');
-  const [btnPayDisabled, setBtnPayDisabled] = useState(false);
-
-  useEffect(() => {
-    if (data) {
-      setShippingByCountry(() => {
-        let groupByCountry = data.allShippings.reduce((acc, cur) => {
-          acc[cur.country.name] = [...(acc[cur.country.name] || []), cur];
-          return acc;
-        }, {});
-        return groupByCountry;
-      });
-    }
-  }, [data]);
-
-  useEffect(() => {
-    updateShippingDetails({
-      country: selectedCountry,
-      state: selectedState,
-      fee: selectedStateFee,
-      address,
-      email,
-      phone
-    });
-    setBtnPayDisabled(
-      !!!(
-        selectedCountry &&
-        selectedState &&
-        selectedStateFee &&
-        address &&
-        email &&
-        phone
-      )
-    );
-  }, [selectedCountry, selectedState, selectedStateFee, address, email, phone]);
 
   return (
     <>
@@ -92,137 +35,14 @@ const CartContainer: FC<CartContainerProps> = ({ checkout }) => {
               />
             </a>
           </Link>
-          <span className='stext-109 cl4'>Shoping Cart</span>
+          <span className='stext-109 cl4'>Shopping Cart</span>
         </div>
       </div>
       <form className='bg0 p-t-75 p-b-85'>
         <div className='container'>
           <div className='row'>
             <CartTable cart={cart} checkout={checkout} />
-            {checkout && cart.length > 0 && (
-              <div className='col-sm-10 col-lg-7 col-xl-5 m-lr-auto m-b-50'>
-                <div className='bor10 p-lr-40 p-t-30 p-b-40 m-l-63 m-r-40 m-lr-0-xl p-lr-15-sm'>
-                  <h4 className='mtext-109 cl2 p-b-30'>Cart Totals</h4>
-                  <div className='flex-w flex-m bor12 p-b-13'>
-                    <div className='size-208'>
-                      <span className='stext-110 cl2'>Shipping Fee:</span>
-                    </div>
-                    <div className='size-209'>
-                      <span className='mtext-110 cl2'>
-                        ₦{selectedStateFee.toFixed(2)}
-                      </span>
-                    </div>
-                  </div>
-                  <div className='flex-w flex-t bor12 p-t-15 p-b-30'>
-                    <div className='size-208 w-full-ssm'>
-                      <span className='stext-110 cl2'>Shipping:</span>
-                    </div>
-                    <div className='size-209 p-r-18 p-r-0-sm w-full-ssm'>
-                      <p className='stext-111 cl6 p-t-2'>
-                        Shipping charges varies based on location
-                      </p>
-                      <div className='p-t-15'>
-                        <span className='stext-112 cl8'>
-                          Calculate Shipping
-                        </span>
-                        <div className='rs1-select2 rs2-select2 bor8 bg0 m-b-12 m-t-9'>
-                          <Select
-                            instanceId='country'
-                            options={Object.keys(shippingByCountry)
-                              .sort()
-                              .map<SelectOptions<string>>(country => ({
-                                label: country,
-                                value: country
-                              }))}
-                            onChange={(e: SelectOptions<string>) =>
-                              setSelectedCountry(e.value)
-                            }
-                            defaultValue={
-                              shippingDetails?.country && {
-                                label: shippingDetails.country,
-                                value: shippingDetails.country
-                              }
-                            }
-                            placeholder='Select a country'
-                          />
-                        </div>
-                        <div className='bor8 bg0 m-b-12'>
-                          <Select
-                            instanceId='state'
-                            options={data?.allShippings.map<
-                              SelectOptions<number>
-                            >(item => ({
-                              label: item.state,
-                              value: item.fee
-                            }))}
-                            onChange={(e: SelectOptions<number>) => {
-                              setSelectedState(e.label);
-                              setSelectedStateFee(e.value);
-                            }}
-                            defaultValue={
-                              shippingDetails?.state &&
-                              shippingDetails?.fee && {
-                                label: shippingDetails.state,
-                                value: shippingDetails.fee
-                              }
-                            }
-                            placeholder='Select a state'
-                          />
-                        </div>
-                        <div className='bor8 bg0 m-b-12'>
-                          <input
-                            className='stext-111 cl8 plh3 size-111 p-lr-15'
-                            type='text'
-                            name='address'
-                            placeholder='Enter your Address'
-                            value={address}
-                            onChange={e => setAddress(e.target.value)}
-                          />
-                        </div>
-                        <div className='bor8 bg0 m-b-12'>
-                          <input
-                            className='stext-111 cl8 plh3 size-111 p-lr-15'
-                            type='email'
-                            name='email'
-                            placeholder='Enter your Email'
-                            value={email}
-                            onChange={e => setEmail(e.target.value)}
-                          />
-                        </div>
-                        <div className='bor8 bg0'>
-                          <input
-                            className='stext-111 cl8 plh3 size-111 p-lr-15'
-                            type='phone'
-                            name='phone'
-                            placeholder='Enter your Phone No.'
-                            value={phone}
-                            onChange={e => setPhone(e.target.value)}
-                          />
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className='flex-w flex-t p-t-27 p-b-33'>
-                    <div className='size-208'>
-                      <span className='mtext-101 cl2'>Total:</span>
-                    </div>
-                    <div className='size-209 p-t-1'>
-                      <span className='mtext-110 cl2'>
-                        ₦
-                        {(
-                          getCartTotalPrice() + shippingDetails?.fee || 0
-                        ).toFixed(2)}
-                      </span>
-                    </div>
-                  </div>
-                  <button
-                    className='flex-c-m stext-101 cl0 size-116 bg3 bor14 hov-btn3 p-lr-15 trans-04 pointer btn-pay'
-                    disabled={btnPayDisabled}>
-                    Proceed to Payment
-                  </button>
-                </div>
-              </div>
-            )}
+            {checkout && cart.length > 0 && <CartCheckout />}
           </div>
         </div>
       </form>
